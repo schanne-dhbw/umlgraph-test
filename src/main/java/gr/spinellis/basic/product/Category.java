a79690aeece691ff28bb01440bf80ba4d088efcf
"Antonio Maria Sanchez" <antoniomariasanchez@gmail.com>
2015-01-12 12:23:36 +0200

    Added more examples

---
+++ src/main/java/gr/spinellis/basic/product/Category.java
@@ -0,0 +1,12 @@
+package gr.spinellis.basic.product;
+import java.util.*;
+
+/**
+ * @depend - - - java.util.List
+ */
+public class Category {
+
+    public String name;
+    List products;
+    private String description;
+}
